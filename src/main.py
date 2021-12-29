791be10eb7aebdbddad67cf384e3bfbd35f105af
Roy Elkabetz <elkabetzroy@gmail.com>
2021-12-29 21:49:32 +0200

    portfolio tests

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import copy as cp
 
 def simulator(liquid, tickers, periods, ratios, sell_strategy, start_date, end_date, buy_fee,
               min_buy_fee, sell_fee, min_sell_fee, tax, verbose, plots_normalize,
-              deposit, deposit_period):
+              deposit, deposit_period, show_plots=True):
 
     traders_list = []
     market = Market(tickers, start_date=start_date, end_date=end_date)
@@ -57,12 +57,13 @@ def simulator(liquid, tickers, periods, ratios, sell_strategy, start_date, end_d
         traders_list.append(trader)
 
     # plot results
-    market_plot(market, normalize=plots_normalize)
-    profit_and_portfolio_value(traders_list, periods, 'balance period')
-    fees_and_tax(traders_list, periods, 'balance period')
-    liquids(traders_list, periods, 'balance period')
-    yields(traders_list, periods, 'balance period', market)
-    yields_usd(traders_list, periods, 'balance period', market, liquid)
+    if show_plots:
+        market_plot(market, normalize=plots_normalize)
+        profit_and_portfolio_value(traders_list, periods, 'balance period')
+        fees_and_tax(traders_list, periods, 'balance period')
+        liquids(traders_list, periods, 'balance period')
+        yields(traders_list, periods, 'balance period', market)
+        yields_usd(traders_list, periods, 'balance period', market, liquid)
 
 
 if __name__ == '__main__':
@@ -84,10 +85,11 @@ if __name__ == '__main__':
     parser.add_argument('-liquid', type=float, required=True)
     parser.add_argument('-verbose', type=bool, default=True)
     parser.add_argument('-plots_normalize', type=bool, default=True)
+    parser.add_argument('-show_plots', type=bool, default=True)
     parser.add_argument('-sell_strategy', type=str, default='FIFO', choices=['FIFO', 'LIFO', 'TAX_OPT'])
     args = parser.parse_args()
 
     # run the simulation
     simulator(args.liquid, args.tickers, args.periods, args.ratios, args.sell_strategy, args.start_date, args.end_date,
               args.buy_fee, args.min_buy_fee, args.sell_fee, args.min_sell_fee, args.tax, args.verbose,
-              args.plots_normalize, args.deposit, args.deposit_period)
+              args.plots_normalize, args.deposit, args.deposit_period, args.show_plots)
--- src/test.py
+++ src/test.py
@@ -1,76 +1,165 @@
-from markets import Market
-from brokers import Broker
-from traders import Trader
 import matplotlib.pyplot as plt
-from utils import plot_trader, compare_traders, plot_market, profit_and_portfolio_value, compare_fees_and_tax
+from main import simulator
+import numpy as np
+from utils import *
 import copy as cp
 
 
-def simulator(liquid, tickers, periods, ratios, sell_strategy, start_date, end_date, buy_fee,
-              min_buy_fee, sell_fee, min_sell_fee, tax, verbose, plots_normalize):
+class Portfolio:
+    def __init__(self, name, tickers, percentages):
+        self.name = name
+        self.tickers = tickers
+        self.ratios = np.array(percentages) / 100.
+        self.traders = None
+        assert np.sum(self.ratios) == 1, 'ratios should sum up to 1.'
+        assert len(self.ratios) == len(self.tickers), 'lengths of ratios and tickers lists should be the same.'
 
-    traders_list = []
-    market = Market(tickers, start_date=start_date, end_date=end_date)
-    broker = Broker(buy_fee=buy_fee, min_buy_fee=min_buy_fee, sell_fee=sell_fee,
-                    min_sell_fee=min_sell_fee, tax=tax, my_market=market)
-    first_date = cp.copy(market.current_date)
-
-    for i, period in enumerate(periods):
-        print(f'period: {period}')
-
-        # init market
-        market.current_idx = 0
-        market.current_date = first_date
-
-        # init new trader
-        trader = Trader(liquid=liquid, balance_period=period, broker=broker, market=market,
-                        verbose=verbose, sell_strategy=sell_strategy)
-
-        # buy some stocks
-        for ticker in tickers:
-            trader.buy(ticker, 1)
-
-        done = False
-        steps = 0
-
-        trader.balance(tickers, p=ratios)
-        while not done:
-            steps += 1
-            if steps % 100 == 0:
-                print('| Step: {:6.0f} / {:6.0f} | Balance period: {:4.0f} |'
-                      .format(steps, market.steps, trader.balance_period))
-            # step market forward in time
-            done, previous_date = market.step()
-
-            # step trader forward in time
-            trader.step(previous_date)
-
-            # balance trader portfolio
-            if steps % trader.balance_period == 0:
-                trader.balance(tickers, p=ratios)
-
-        traders_list.append(trader)
-
-    # # plot market
-    # plot_market(market, normalize=plots_normalize)
-    return traders_list[0]
+    def add_traders(self, traders_list):
+        self.traders = traders_list
 
 
 if __name__ == '__main__':
+    # periods to simulation
+    periods = [30]
 
-    # compare FIFO, LIFO, and Tax minimization strategy
-    traders_list = []
-    prm = ['FIFO']
+    # Simulator arguments
+    liquid = 100000
+    sell_strategy = 'TAX_OPT'
+    start_date = (2010, 1, 1)
+    end_date = (2021, 11, 1)
+    buy_fee = 0
+    min_buy_fee = 1.
+    sell_fee = 0
+    min_sell_fee = 1.
+    tax = 25.
+    verbose = False
+    plots_normalize = False
+    deposit = 0
+    deposit_period = 0
+    show_plots = False
 
-    # run the simulation
-    for strategy in prm:
-        print(f'strategy: {strategy}')
-        trader = simulator(100000., ['AAPL', 'GOOG', 'SPY', 'ORCL'], [10], [0.25] * 4, strategy,
-                           (2018, 1, 1), (2021, 4, 20), 0.08, 2., 0.08, 2., 25., False, True)
-        traders_list.append(trader)
+    # portfolios to simulate
+    portfolio1 = Portfolio('Ideal Index',
+                           ['SPY', 'VBK', 'IUSV', 'VBR', 'VNQ', 'VXUS', 'BSV'],
+                           [6.25, 6.25, 9.25, 9.25, 8, 31, 40])
+    portfolio2 = Portfolio('Harry Browne',
+                           ['VTI', 'GLD', 'TLT', 'SHV'],
+                           [25., 25., 25., 25.])
+    portfolio3 = Portfolio('Warren Buffett',
+                           ['SPY', 'BND'],
+                           [90., 10.])
+    portfolio4 = Portfolio('All Seasons',
+                           ['VTI', 'IEF', 'TLT', 'DBC', 'GLD'],
+                           [30., 15., 40., 7.5, 7.5])
+    portfolio5 = Portfolio('Stocks 100%',
+                           ['VT'],
+                           [100.])
+    portfolio6 = Portfolio('2nd Grade',
+                           ['VTI', 'VXUS', 'BND'],
+                           [60., 30., 10.])
+    portfolio7 = Portfolio('No Brainer',
+                           ['VTI', 'VB', 'VEA', 'BND'],
+                           [25., 25., 25., 25.])
+    portfolio8 = Portfolio('Three Funds',
+                           ['VTI', 'VXUS', 'BND'],
+                           [40., 20., 40.])
+    portfolio9 = Portfolio('Ivy League',
+                           ['VTI', 'VXUS', 'BND', 'VNQ', 'DBC'],
+                           [20., 20., 20., 20., 20.])
+    portfolio10 = Portfolio('Four Cores',
+                            ['VTI', 'VXUS', 'VNQ', 'BND'],
+                            [48., 24., 8., 20.])
+    portfolio11 = Portfolio('Margarita',
+                            ['VTI', 'VXUS', 'BND'],
+                            [33.3, 33.3, 33.4])
+    portfolio12 = Portfolio('Went Fishing',
+                            ['VTI', 'VB', 'VGK', 'VPL', 'VWO', 'SHY', 'TIP', 'JNK', 'VNQ', 'GLTR'],
+                            [15., 15., 10., 10., 10., 10., 10., 5., 5.])
+    portfolio13 = Portfolio('50/50',
+                            ['VT', 'BND'],
+                            [50., 50.])
+    portfolio14 = Portfolio('Foursquare',
+                            ['VTI', 'VXUS', 'BND', 'BNDX'],
+                            [25., 25., 25., 25.])
+    portfolio15 = Portfolio('Unconventional Success',
+                            ['VTI', 'VEA', 'VWO', 'TLT', 'TIP', 'VNQ'],
+                            [20., 20., 10., 15., 15., 20.])
+    portfolio16 = Portfolio('Dark Chocolate',
+                            ['VEA', 'VWO', 'TIP', 'BSV'],
+                            [55.25, 9.75, 17.50, 17.50])
+    portfolio17 = Portfolio('Cherry Pie',
+                            ['TLT', 'VWO', 'GLD', 'VBR', 'VNQ', 'BSV'],
+                            [12., 12., 12., 12., 12., 40.])
+    portfolio18 = Portfolio('Colliding Markets',
+                            ['VTI', 'VEA', 'VWO', 'TLT', 'BNDX', 'TIP', 'VNQ', 'DBC'],
+                            [18., 18., 15., 6., 11., 6., 13., 13.])
+    portfolio19 = Portfolio('Ultimate Buy & Hold',
+                            ['SPY', 'IUSV', 'VB', 'VBR', 'VEA', 'VWO', 'EFV', 'VSS', 'VNQ', 'SHY', 'IEF'],
+                            [6., 6., 6., 6., 6., 6., 6., 12., 6., 12.])
+    portfolio20 = Portfolio('Coffee Shop',
+                            ['SPY', 'VB', 'IUSV', 'VBR', 'VNQ', 'VXUS', 'BND'],
+                            [10., 10., 10., 10., 10., 10., 40.])
+    portfolio21 = Portfolio('Chicken',
+                            ['SPY', 'VB', 'IUSV', 'VBR', 'VNQ', 'VGK', 'VPL', 'VWO', 'SHY'],
+                            [15., 5., 10., 10., 5., 5., 5., 5., 40.])
+    portfolio22 = Portfolio('Big Bricks',
+                            ['SPY', 'VB', 'VTV', 'VBR', 'VNQ', 'VXUS', 'EFV', 'VSS', 'DLS', 'VWO', 'BSV'],
+                            [9., 9., 9., 9., 6., 3., 6., 3., 3., 3., 40.])
+    portfolio23 = Portfolio('7-12',
+                            ['VOO', 'VO', 'VB', 'VEA', 'VWO', 'SHV', 'BND', 'TIP', 'BNDX', 'VNQ', 'DBC', 'IGE'],
+                            [8.3, 8.3, 8.3, 8.3, 8.3, 8.3, 8.3, 8.3, 8.3, 8.3, 8.3, 8.3])
+    portfolio24 = Portfolio('Thick Tail',
+                            ['VBR', 'BSV'],
+                            [32., 68.])
+    portfolio25 = Portfolio('Talmud',
+                            ['VT', 'RWO', 'BND'],
+                            [33.3, 33.3, 33.4])
 
-    # plot results
-    compare_fees_and_tax(traders_list, prm, 'strategy')
-    profit_and_portfolio_value(traders_list, prm, 'strategy')
+    # portfolios_list = [portfolio1, portfolio2, portfolio3, portfolio4, portfolio5, portfolio6, portfolio7,
+    #                    portfolio8, portfolio9, portfolio10, portfolio11, portfolio12, portfolio13, portfolio14,
+    #                    portfolio15, portfolio16, portfolio17, portfolio18, portfolio19, portfolio20, portfolio21,
+    #                    portfolio22, portfolio23, portfolio24, portfolio25
+    #                    ]
+
+    portfolios_list = [portfolio1, portfolio2
+                       ]
+
+    for i, portfolio in enumerate(portfolios_list):
+        # run simulator
+        traders_list = simulator(liquid, portfolio.tickers, periods, portfolio.ratios, sell_strategy, start_date,
+                                 end_date, buy_fee, min_buy_fee, sell_fee, min_sell_fee, tax, verbose,
+                                 plots_normalize, deposit, deposit_period, show_plots)
+
+        # save results
+        portfolio.add_traders(traders_list)
+
+    interval = np.int(len(portfolio.traders[0].date_history) / 10)
+    fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, dpi=150)
+    axes.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    axes.xaxis.set_major_locator(mdates.DayLocator(interval=interval))
+    axes.set_title('Value history')
+    for i, portfolio in enumerate(portfolios_list):
+        axes.plot(portfolio.traders[0].date_history, portfolio.traders[0].portfolio_value_history, label=portfolio.name)
+
+    axes.set_ylabel('USD')
+    fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
+    axes.legend()
+    axes.grid()
+    plt.show()
+
+    interval = np.int(len(portfolio.traders[0].date_history) / 10)
+    fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, dpi=150)
+    axes.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    axes.xaxis.set_major_locator(mdates.DayLocator(interval=interval))
+    axes.set_title('Yield history')
+    for i, portfolio in enumerate(portfolios_list):
+        axes.plot(portfolio.traders[0].date_history, portfolio.traders[0].yield_history, label=portfolio.name)
+
+    axes.set_ylabel('[%]')
+    fig.autofmt_xdate(bottom=0.2, rotation=30, ha='right')
+    axes.legend()
+    axes.grid()
+    plt.show()
+
 
 
